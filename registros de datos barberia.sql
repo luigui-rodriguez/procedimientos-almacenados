f06a22475bb18df2631e34bfb32ab891763e6f3a
luigui-rodriguez <99194536+luigui-rodriguez@users.noreply.github.com>
2022-05-27 21:36:28 -0500

    Initial commit

---
+++ registros de datos barberia.sql
@@ -0,0 +1,272 @@
+use BD_CWS;
+
+insert into Cliente values (1011258690, 'daniel hernandez', 'Carolina@gmail.com', 3108053, 'Calle 10 # 5-51');
+insert into Cliente values (1017197473, 'Marcela Rey', 'Marcela@gmail.com', 3102478, 'Calle 53 No 10-60');
+insert into Cliente values (1018374140, 'Alejandro Abondano', 'Alejandro@gmail.com', 3107854, 'Calle 10 # 5-51');
+insert into Cliente values (1020399469, 'Alexander Carvajal', 'Alexander@gmail.com', 3103156, 'Calle 9 # 9 – 62');
+insert into Cliente values (1020419185, 'Catalina Acero', 'Catalina@gmail.com', 3102145, 'Calle 19 # 80A-40');
+insert into Cliente values (1020421293, 'Liliana Cruz', 'Liliana@gmail.com', 3100318, 'Carrera 21 # 17 -63');
+insert into Cliente values (1000130311, 'Felipe Villa', 'Felipe@gmail.com', 31033625, 'Carrera 42 # 54-77');
+insert into Cliente values (1020435242, 'Patricia Maecha', 'Patriciagmail.com', 3109015, 'Calle 100 # 11B-27');
+insert into Cliente values (1020435259, 'Lisseth Blanco ', 'Lisseth@gmail.com', 3102215, 'Carrera 20 B # 29-18');
+insert into Cliente values (1020442012, 'Maria Rocha', 'Maria@gmail.com', 3103334, 'Transversal 9 a No. 29 - 29');
+insert into Cliente values (1026140391, 'Tatiana Fernandez', 'Tatiana@gmail.com', 3107855, 'Calle 7 # 19-35');
+insert into Cliente values (1035418293, 'Brigite Polanco', 'Brigite@gmail.com', 3107848, 'Carrera 3 # 18- 45');
+insert into Cliente values (1035421262, 'Camilo Villamizar', 'Camilo@gmail.com', 3107744, 'Carrera 7 # 84- 72');
+insert into Cliente values (1035850508, 'Camilo Rodriguez', 'Camilo1@gmail.com', 3100215, 'Calle 4 No. 5 – 10');
+insert into Cliente values (1035853694, 'Alberto Cortés', 'Alberto@gmail.com', 3107033, 'Calle 25 #45-30');
+insert into Cliente values (1035858066, 'Enrique Gomez', 'Enrique@gmail.com', 3101122, 'Calle 11 No. 4 - 14');
+insert into Cliente values (1036394490, 'Andrés Polo', 'Andrés@gmail.com', 3101312, 'Av. Ciudad de Cali No. 6C-09');
+insert into Cliente values (1036603880, 'Didier Castaño', 'Didier@gmail.com', 3109514, 'Calle 48b sur No. 21-13');
+insert into Cliente values (1036603893, 'Felipe Mogollon', 'Felipe@gmail.com', 3107530, 'Avenida Cra. 60 No. 57-60');
+insert into Cliente values (1036604697, 'Ruchina Gomez', 'Ruchina@gmail.com', 3100258, 'Calle 11 No. 4-21 / 93');
+insert into Cliente values (1036607340, 'Carolina Pintor', 'Carolina@gmail.com', 3100225, 'Calle 10 No. 5-22');
+insert into Cliente values (1036607768, 'Ruchi Perez', 'Ruchi1@gmail.com', 3100124, 'Carrera 20 No. 37-54');
+insert into Cliente values (1036609429, 'Carol Pintor', 'Carol1@gmail.com', 3109317, 'Av. Calle 19 N° 2-49 Centro');
+insert into Cliente values (1036610160, 'Catherine Ospina', 'Catherine@gmail.com', 3107139, 'Calle 109a N° 17-10 Sede Norte');
+insert into Cliente values (1036610289, 'Fernanda Dussán', 'Fernanda@gmail.com', 3100992, 'Cr.8a # 15-63');
+insert into Cliente values (1036610490, 'Liliana Tporres', 'Liliana1@gmail.com', 3102322, 'Cr.9 # 74-99');
+insert into Cliente values (1036612110, 'Elizabeth Barthel', 'Elizabeth@gmail.com', 3109155, 'Carrera 1 Este No. 17 01');
+insert into Cliente values (1036614376, 'Daniel Gómez', 'Daniel@gmail.com', 3101351, 'Cr.6 # 17-12');
+insert into Cliente values (1036615280, 'Andrés Castiblanco', 'Andrés1@gmail.com', 3107748, 'Carrera 7 Calle 26');
+insert into Cliente values (1036615563, 'Daniela Hernández', 'Daniela gmail.com', 3109968, 'Av.19 #152-48 Esquina');
+
+select * from Cliente;
+
+insert into Barbero values (1036615564, 'Lusi Miguel', 'Luis@gmail.com', 3102548);
+insert into Barbero values (1036615565, 'Pablo Neruda', 'Pablo@gmail.com', 3132215);
+insert into Barbero values (1036615566, 'Leo Dan', 'Leo@gmail.com', 3140015);
+insert into Barbero values (1036615567, 'Enrique Iglesias', 'Enrique@gmail.com', 3113205);
+insert into Barbero values (1036615568, 'Santiago Cruz', 'Santiago@gmail.com', 3127845);
+
+select * from Barbero;
+
+insert into Administrador values (1000862842, 'Sebastian Forero', 'SebastianF@gmail.com', 3108069);
+insert into Administrador values (1000862823, 'Luigi Rodriguez', 'LuigiR@gmail.com', 3058538);
+insert into Administrador values (1000862854, 'Gorgy Casallas', 'GorgyC@gmail.com', 3144506);
+
+Select * from Administrador;
+
+insert into Producto values (1, 'Shampoo Natural Para Niños', 60.000);
+insert into Producto values (2, 'Acondicionador De Guácimo 500ml', 29.000);
+insert into Producto values (3, 'Cepillo Andis', 27.000);
+insert into Producto values (4, 'Talco Corporal Barberia Clubman', 32.000);
+insert into Producto values (5, 'Aceite Barba Clubman', 29.000);
+insert into Producto values (6, 'Crema Refrescante', 51.000);
+insert into Producto values (7, 'Pomada De Fibra', 155.000);
+insert into Producto values (8, 'Kit De Rasurado Tradicional', 38.000);
+insert into Producto values (9, 'Brocha Talquera', 7.000);
+insert into Producto values (10, 'Crema Para Afeitar', 131.000);
+insert into Producto values (11, 'Afeitadora Desechable', 174.000);
+insert into Producto values (12, 'Jabon De Afeitar', 109.000);
+insert into Producto values (13, 'Cuenco De Acero', 152.000);
+insert into Producto values (14, 'Tijeras Pelo', 88.000);
+insert into Producto values (15, 'Brocha De Afeitar', 214.000);
+insert into Producto values (16, 'Tinte Inoa Permanente', 27.000);
+insert into Producto values (17, 'Cera Depilatoria', 15.000);
+insert into Producto values (18, 'Cera Barbershop', 18.000);
+insert into Producto values (19, 'Kit Keratina', 52.000);
+insert into Producto values (20, 'Ampolletas Essential', 78.000);
+insert into Producto values (21, 'Tratamiento Reparador', 114.000);
+insert into Producto values (22, 'Laca Spray', 11.000);
+insert into Producto values (23, 'Carbón Activado', 4.000);
+insert into Producto values (24, 'Versace Eros', 40.000);
+insert into Producto values (25, 'Kuul Gel', 16.000);
+insert into Producto values (26, 'Crema Hidratante', 125.000);
+insert into Producto values (27, 'Aceite Moroccanoil', 71.000);
+insert into Producto values (28, 'Moñas 24 Unidades', 9.000);
+insert into Producto values (29, 'Hoja Afeitar X 60', 17.000);
+
+select * from Producto;
+
+insert into Servicios values (1, 'Corte caballero', 28.000);
+insert into Servicios values (2, 'Ritual barba', 20.000);
+insert into Servicios values (3, 'Corte y barba Caballero', 48.000);
+insert into Servicios values (4, 'Corte niño', 25.000);
+insert into Servicios values (5, 'Shampoo+Peinado', 12.000);
+insert into Servicios values (6, 'Peinado', 28.000);
+insert into Servicios values (7, 'Mascarilla de colájeno', 17.000);
+insert into Servicios values (8, 'Mascarilla negra removedora de puntos', 14.000);
+insert into Servicios values (9, 'Exfolación facial', 14.000);
+insert into Servicios values (10, 'Depilación cejas con depiladora', 7.000);
+insert into Servicios values (11, 'Depilación cejas con cuchilla', 4.000);
+insert into Servicios values (12, 'Depilacion nasal o orejas', 7.000);
+insert into Servicios values (13, 'Teusaquillo', 60.000);
+insert into Servicios values (14, 'Candelaria', 55.000);
+insert into Servicios values (15, 'Monserrate', 50.000);
+insert into Servicios values (16, 'Manicure', 12.000);
+insert into Servicios values (17, 'Pedicure', 16.000);
+insert into Servicios values (18, 'Tinture de cabello', 16.000);
+insert into Servicios values (19, 'Lavado de cabello', 6.000);
+insert into Servicios values (20, 'Extraccion de espinillas', 16.000);
+
+select * from Servicios;
+
+insert into Proveedores values (1036615564, 'Andis', 'Andis@gmail.com', 'Carrera 30 No. 20-45', 3144518);
+insert into Proveedores values (1036615565, 'Clubman', 'Clubman@gmail.com', 'Carrera 31 No. 21-46', 3144519);
+insert into Proveedores values (1036615566, 'Guácimo', 'Guácimo@gmail.com', 'Carrera 32 No. 22-47', 3144520);
+insert into Proveedores values (1036615567, 'Inoa', 'Inoa@gmail.com', 'Carrera 33 No. 23-48', 3144521);
+insert into Proveedores values (1036615568, 'Essential', 'Essential@gmail.com', 'Carrera 34 No. 24-49', 3144522);
+insert into Proveedores values (1036615569, 'AnErosdis', 'Eros@gmail.com', 'Carrera 35 No. 25-50', 3144523);
+insert into Proveedores values (1036615570, 'Kuul', 'Kuul@gmail.com', 'Carrera 36 No. 26-51', 3144524);
+insert into Proveedores values (1036615571, 'Moroccanoil', 'Moroccanoil@gmail.com', 'Carrera 37 No. 27-52', 3144525);
+insert into Proveedores values (1036615572, 'Hunter Store', 'Hunter_Store@gmail.com', 'Carrera 38 No. 28-53', 3144526);
+
+select * from Proveedores;
+
+insert into Facturas values (1, '2022-01-01', 1017195379);
+insert into Facturas values (2, '2022-01-01', 1017197473);
+insert into Facturas values (3, '2022-01-01', 1018374140);
+insert into Facturas values (4, '2022-01-01', 1020399469);
+insert into Facturas values (5, '2022-01-01', 1020419185);
+
+insert into Facturas values (6, '2022-01-02', 1020421293);
+insert into Facturas values (7, '2022-01-02', 1000130311);
+insert into Facturas values (8, '2022-01-02', 1020435242);
+insert into Facturas values (9, '2022-01-02', 1020435259);
+insert into Facturas values (10, '2022-01-02', 1020442012);
+
+insert into Facturas values (11, '2022-01-03', 1026140391);
+insert into Facturas values (12, '2022-01-03', 1035418293);
+insert into Facturas values (13, '2022-01-03', 1035421262);
+insert into Facturas values (14, '2022-01-03', 1035850508);
+insert into Facturas values (15, '2022-01-03', 1035853694);
+
+insert into Facturas values (16, '2022-01-04', 1035858066);
+insert into Facturas values (17, '2022-01-04', 1036394490);
+insert into Facturas values (18, '2022-01-04', 1036603880);
+insert into Facturas values (19, '2022-01-04', 1036603893);
+insert into Facturas values (20, '2022-01-04', 1036604697);
+
+insert into Facturas values (21,'2022-01-05', 1036607340);
+insert into Facturas values (22, '2022-01-05', 1036607768);
+insert into Facturas values (23, '2022-01-05', 1036609429);
+insert into Facturas values (24, '2022-01-05', 1036610160);
+insert into Facturas values (25, '2022-01-05', 1036610289);
+
+insert into Facturas values (26, '2022-01-06', 1036610490);
+insert into Facturas values (27, '2022-01-06', 1036612110);
+insert into Facturas values (28, '2022-01-06', 1036614376);
+insert into Facturas values (29, '2022-01-06', 1036615280);
+insert into Facturas values (30, '2022-01-06', 1036615563);
+
+select * from Facturas;
+
+insert into Agenda values (1, 100, '2022-03-06', '02:00:00.00');
+insert into Agenda values (2, 100, '2022-03-07', '03:00:00.0');
+insert into Agenda values (3, 100, '2022-03-08', '04:00:00.00');
+insert into Agenda values (4, 100, '2022-03-09', '05:00:00.00');
+insert into Agenda values (5, 100, '2022-03-10', '06:00:00.00');
+insert into Agenda values (6, 100, '2022-03-11', '07:00:00.00');
+insert into Agenda values (7, 100, '2022-03-12', '08:00:00.00');
+
+insert into Agenda values (8, 101, '2022-03-06', '02:00:00.00');
+insert into Agenda values (9, 101, '2022-03-07', '03:00:00.00');
+insert into Agenda values (10, 101, '2022-03-08', '04:00:00.00');
+insert into Agenda values (11, 101, '2022-03-09', '05:00:00.0');
+insert into Agenda values (12, 101, '2022-03-10', '06:00:00.00');
+insert into Agenda values (13, 101, '2022-03-11', '07:00:00.00');
+insert into Agenda values (14, 101, '2022-03-12', '08:00:00.00');
+
+insert into Agenda values (15, 102, '2022-03-06', '02:00:00.00');
+insert into Agenda values (16, 102, '2022-03-07', '03:00:00.00');
+insert into Agenda values (17, 102, '2022-03-08', '04:00:00.00');
+insert into Agenda values (18, 102, '2022-03-09', '05:00:00.00');
+insert into Agenda values (19, 102, '2022-03-10', '06:00:00.00');
+insert into Agenda values (20, 102, '2022-03-11', '07:00:00.00');
+insert into Agenda values (21, 102, '2022-03-12', '08:00:00.00');
+
+insert into Agenda values (22, 103, '2022-03-06', '02:00:00.00');
+insert into Agenda values (23, 103, '2022-03-07', '03:00:00.00');
+insert into Agenda values (24, 103, '2022-03-08', '04:00:00.00');
+insert into Agenda values (25, 103, '2022-03-09', '05:00:00.00');
+insert into Agenda values (26, 103, '2022-03-10', '06:00:00.00');
+insert into Agenda values (27, 103, '2022-03-11', '07:00:00.00');
+insert into Agenda values (28, 103, '2022-03-12', '08:00:00.00');
+
+insert into Agenda values (29, 104, '2022-03-06', '02:00:00.00');
+insert into Agenda values (30, 104, '2022-03-07', '03:00:00.00');
+insert into Agenda values (31, 104, '2022-03-08', '04:00:00.00');
+insert into Agenda values (32, 104, '2022-03-09', '05:00:00.00');
+insert into Agenda values (33, 104, '2022-03-10', '06:00:00.00');
+insert into Agenda values (34, 104, '2022-03-11', '07:00:00.00');
+insert into Agenda values (35, 104, '2022-03-12', '08:00:00.00');
+
+insert into Agenda values (36, 105, '2022-03-06', '02:00:00.00');
+insert into Agenda values (37, 105, '2022-03-07', '03:00:00.00');
+insert into Agenda values (38, 105, '2022-03-08', '04:00:00.00');
+insert into Agenda values (39, 105, '2022-03-09', '05:00:00.00');
+insert into Agenda values (40, 105, '2022-03-10', '06:00:00.00');
+insert into Agenda values (41, 105, '2022-03-11', '07:00:00.00');
+insert into Agenda values (42, 105, '2022-03-12', '08:00:00.00');
+
+select * from Agenda;
+
+insert into Venta values (1, 1017195379, '2022-03-06', 'P100', 'S100', 88.000);
+insert into Venta values (2, 1017197473, '2022-03-06', 'P101', 'S101', 49.000);
+insert into Venta values (3, 1018374140, '2022-03-06', 'P102', 'S102', 75.000);
+insert into Venta values (4, 1020399469, '2022-03-06', 'P103', 'S103', 57.000);
+insert into Venta values (5, 1020419185, '2022-03-06', 'P104', 'S104', 41.000);
+insert into Venta values (6, 1020421293, '2022-03-06', 'P105', 'S105', 79.000);
+insert into Venta values (7, 1000130311, '2022-03-06', 'P106', 'S106', 172.000);
+insert into Venta values (8, 1020435242, '2022-03-06', 'P107', 'S107', 52.000);
+insert into Venta values (9, 1020435259, '2022-03-06', 'P108', 'S108', 21.000);
+insert into Venta values (10, 1020442012, '2022-03-06', 'P109', 'S109', 138.000);
+insert into Venta values (11, 1026140391, '2022-03-06', 'P110', 'S110', 178.000);
+insert into Venta values (12, 1035418293, '2022-03-06', 'P111', 'S111', 116.000);
+insert into Venta values (13, 1035421262, '2022-03-06', 'P112', 'S112', 212.000);
+insert into Venta values (14, 1035850508, '2022-03-06', 'P113', 'S113', 135.000);
+insert into Venta values (15, 1035853694, '2022-03-06', 'P114', 'S114', 264.000);
+insert into Venta values (16, 1035858066, '2022-03-06', 'P115', 'S115', 39.000);
+insert into Venta values (17, 1036394490, '2022-03-06', 'P116', 'S116', 31.000);
+insert into Venta values (18, 1036603880, '2022-03-06', 'P117', 'S117', 46.000);
+insert into Venta values (19, 1036603893, '2022-03-06', 'P118', 'S100', 72.000);
+insert into Venta values (20, 1036604697, '2022-03-06', 'P119', 'S101', 126.000);
+insert into Venta values (21, 1036607340, '2022-03-06', 'P120', 'S102', 139.000);
+insert into Venta values (22, 1036607768, '2022-03-06', 'P121', 'S103', 23.000);
+insert into Venta values (23, 1036609429, '2022-03-06', 'P122', 'S104', 32.000);
+insert into Venta values (24, 1036610160, '2022-03-06', 'P123', 'S105', 57.000);
+insert into Venta values (25, 1036610289, '2022-03-06', 'P124', 'S106', 30.000);
+insert into Venta values (26, 1036610490, '2022-03-06', 'P125', 'S107', 139.000);
+insert into Venta values (27, 1036612110, '2022-03-06', 'P126', 'S108', 78.000);
+insert into Venta values (28, 1036614376, '2022-03-06', 'P127', 'S109', 13.000);
+insert into Venta values (29, 1036615280, '2022-03-06', 'P128', 'S110', 24.000);
+insert into Venta values (30, 1036615563, '2022-03-06', 'P100', 'S111', 64.000);
+
+select * from Venta;
+
+insert into Compra values (1036615564, 'Andis', 100, 27.000);
+insert into Compra values (1036615565, 'Clubman', 200, 61.000);
+insert into Compra values (1036615566, 'Guácimo', 150, 29.000);
+insert into Compra values (1036615567, 'Inoa', 100, 27.000);
+insert into Compra values (1036615568, 'Essential', 200, 78.000);
+insert into Compra values (1036615569, 'AnErosdis', 150, 40.000);
+insert into Compra values (1036615570, 'Kuul', 50, 16.000);
+insert into Compra values (1036615571, 'Moroccanoil', 100, 71.000);
+insert into Compra values (1036615572, 'Hunter Store', 50, 26.000);
+
+select * from Compra;
+
+insert into Inventario values (1, 1036615565, 'Andis', 100, '2022-01-01', '2022-03-31', 27.000);
+insert into Inventario values (2, 1036615564, 'Clubman', 200, '2022-01-01', '2022-03-31', 61.000);
+insert into Inventario values (3, 1036615566, 'Guácimo', 100, '2022-01-01', '2022-03-31', 29.000);
+insert into Inventario values (4, 1036615567, 'Inoa', 100, '2022-01-01', '2022-03-31', 27.000);
+insert into Inventario values (5, 1036615568, 'Essential', 200, '2022-01-02', '2022-03-31', 40.000);
+insert into Inventario values (6, 1036615569, 'AnErosdis', 150, '2022-01-02', '2022-03-31', 78.000);
+insert into Inventario values (7, 1036615570, 'Kuul', 50, '2022-01-03', '2022-03-31', 40.000);
+insert into Inventario values (8 ,1036615571, 'Moroccanoil', 100, '2022-01-03', '2022-03-31', 71.000);
+insert into Inventario values (9, 1036615572, 'Hunter Store', 50, '2022-01-03', '2022-03-31', 26.000);
+
+select * from Inventario;
+
+insert into Cita values (1, 1000, 'S100');
+insert into Cita values (2, 1000, 'S106');
+insert into Cita values (3, 1000, 'S105');
+insert into Cita values (4, 1000, 'S104');
+
+insert into Cita values (5, 1001, 'S108');
+insert into Cita values (6, 1001, 'S111');
+insert into Cita values (7, 1001, 'S108');
+insert into Cita values (8, 1001, 'S112');
+
+select * from Cita;
